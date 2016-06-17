13e943d5b3247459199f2f27332dd291eb4f16ff
LeNPaul <l.nguyen.paul@gmail.com>
2016-06-16 23:47:01 -0400

    Update personal portfolio site

--- HTML/personal-portfolio-webpage/js/index.js
+++ HTML/personal-portfolio-webpage/js/index.js
@@ -31,3 +31,26 @@
   window.addEventListener("scroll", callbackFunc);
 
 })();
+
+(function ($) {
+  $(document).ready(function(){
+    
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 300) {
+                $('.navbar').fadeIn();
+              $('.navbar').addClass('navbar-fixed');
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
