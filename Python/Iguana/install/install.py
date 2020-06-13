909e26b7adb0a33e84ec37edb8b3b70632a0c5aa
Paul Le <l.nguyen.paul@gmail.com>
2020-06-13 01:32:36 -0400

    Add logic to start and stop Iguana process

--- Python/Iguana/install/install.py
+++ Python/Iguana/install/install.py
@@ -1,7 +1,13 @@
-import shutil
+import os
 
 iguanas = ['1','2','3','4','5','6','7','8','9','10']
 
+f = open('test.txt', 'wb')
+f.write('hello world')
+f.close()
+
 for x in iguanas:
-	shutil.copytree('iNTERFACEWARE-Iguana', '../iguana-' + x)
-	shutil.copytree('IguanaMainRepo', '../iguana-' + x + '/IguanaMainRepo')
+        shutil.copytree('iNTERFACEWARE-Iguana', '../iguana-' + x)
+        shutil.copytree('IguanaMainRepo', '../iguana-' + x + '/IguanaMainRepo')
+        os.system('cd ~/iguana-' + x + '; ./iguana_service')
+        os.system('sudo pkill -SIGTERM iguana_service')
