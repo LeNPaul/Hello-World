8e902e623b39e4f1a871cc1fa95a5581afdfb6fe
Paul Le <l.nguyen.paul@gmail.com>
2020-06-16 15:07:08 -0400

    Update install.py

--- Python/Iguana/install/install.py
+++ Python/Iguana/install/install.py
@@ -1,13 +1,27 @@
+import shutil
 import os
+import time
 
-iguanas = ['1','2','3','4','5','6','7','8','9','10']
-
-f = open('test.txt', 'wb')
-f.write('hello world')
-f.close()
+iguanas = ['01','02','03','04','05','06','07','08','09','10']
 
 for x in iguanas:
         shutil.copytree('iNTERFACEWARE-Iguana', '../iguana-' + x)
         shutil.copytree('IguanaMainRepo', '../iguana-' + x + '/IguanaMainRepo')
         os.system('cd ~/iguana-' + x + '; ./iguana_service')
+        time.sleep(3)
+
         os.system('sudo pkill -SIGTERM iguana_service')
+        time.sleep(3)
+
+        fin = open('/home/centos/iguana-' + x + '/IguanaConfigurationRepo/IguanaConfiguration.xml', 'rt')
+        config = fin.read()
+        config = config.replace('port="6543"', 'port="654' + x + '"')
+        fin.close()
+
+        fin = open('/home/centos/iguana-' + x + '/IguanaConfigurationRepo/IguanaConfiguration.xml', 'wt')
+        fin.write(config)
+        fin.close()
+
+        os.rename('/home/centos/iguana-' + x + '/iguana_service', '/home/centos/iguana-' + x + '/iguana_service_' + x)
+        os.rename('/home/centos/iguana-' + x + '/iguana_service.hdf', '/home/centos/iguana-' + x + '/iguana_service_' + x + '.hdf')
+        os.system('cd ~/iguana-' + x + '; ./iguana_service_' + x)
